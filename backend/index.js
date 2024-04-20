b3c79f64457831ffebfa30d7efe085d07868b886
Vibhu Rathore <rathorevibhu10@gmail.com>
2024-04-20 18:31:55 +0530

    enable cors on backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const routeHandlers = require("./route_handlers");
 
-const PORT = 3000;
+const PORT = 8080;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
