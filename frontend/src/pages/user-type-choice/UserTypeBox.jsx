65701b01d77bf34aafa006a2783233dc229b1eff
Vibhu Rathore <rathorevibhu10@gmail.com>
2024-04-20 20:26:51 +0530

    user type box styles

--- frontend/src/pages/user-type-choice/UserTypeBox.jsx
+++ frontend/src/pages/user-type-choice/UserTypeBox.jsx
@@ -22,15 +22,26 @@ const UserTypeBox = ({ text, image, name, value, onChange }) => {
       <div className="aspect-square flex items-center">
         <img src={image} className="object-contain w-full h-full" />
       </div>
-      <p className="font-semibold">{text}</p>
-      <span
-        className={`w-4 h-4 inline-flex justify-center items-center text-white rounded-full m-1 ${
-          checked ? "bg-pink-400" : "outline-stone-300 outline-1 outline-double"
-        } shrink-0`}
-      >
-        {checked && <Check />}
-      </span>
-      <input type="checkbox" name={name} value={value} checked={checked} readOnly className="hidden" />
+      <div className="grow flex flex-col items-center justify-between">
+        <p className="font-semibold">{text}</p>
+        <span
+          className={`w-4 h-4 inline-flex justify-center items-center text-white rounded-full m-1 ${
+            checked
+              ? "bg-pink-400"
+              : "outline-stone-300 outline-1 outline-double"
+          } shrink-0 p-0.5`}
+        >
+          {checked && <Check strokeWidth={4} />}
+        </span>
+      </div>
+      <input
+        type="checkbox"
+        name={name}
+        value={value}
+        checked={checked}
+        readOnly
+        className="hidden"
+      />
     </label>
   );
 };
