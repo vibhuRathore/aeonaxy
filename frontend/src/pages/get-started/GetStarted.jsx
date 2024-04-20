259a4a0341e683a753f5c1321c228af8cb0eb7fc
Vibhu Rathore <rathorevibhu10@gmail.com>
2024-04-20 19:36:54 +0530

    get started page styles

--- frontend/src/pages/get-started/GetStarted.jsx
+++ frontend/src/pages/get-started/GetStarted.jsx
@@ -24,7 +24,7 @@ const GetStarted = () => {
   };
 
   return (
-    <div className="w-1/2 my-0 mx-auto">
+    <div className="w-1/2 mt-14 mx-auto">
       <div className="mb-7">
         <h3 className="text-3xl font-bold">
           Welcome! Let's create your profile
@@ -34,14 +34,14 @@ const GetStarted = () => {
         </p>
       </div>
 
-      <form className="flex flex-col gap-7" onSubmit={handleSubmit}>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="avatar" className="text-lg font-bold">
+          <label htmlFor="avatar" className="text-lg font-bold block mb-3">
             Add an avatar
           </label>
           <ImageUpload onImageChange={onImageChange} />
         </div>
-        <div>
+        <div className="mt-6">
           <label label="location" className="text-lg font-bold">
             Add your location
           </label>
@@ -50,12 +50,12 @@ const GetStarted = () => {
             name="location"
             id="location"
             placeholder="Enter a location"
-            className="w-full bg-transparent border-b-2 outline-none py-2"
+            className="w-full bg-transparent border-b-[2px] outline-none py-2"
             value={userLocation}
             onChange={(evt) => setUserLocation(evt.target.value)}
           />
         </div>
-        <Button type="submit" text="Next" />
+        <Button type="submit" text="Next" className="mt-3" />
       </form>
     </div>
   );
